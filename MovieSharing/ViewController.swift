137f15f5b1052b0fffb2878d76ba9282b2657b96
Shruti <shruti.upadhya@gmail.com>
2020-09-18 15:38:19 +0200

    Initial Commit

---
+++ MovieSharing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSharing
+//
+//  Created by Shruti on 18/09/20.
+//  Copyright © 2020 Shruti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
